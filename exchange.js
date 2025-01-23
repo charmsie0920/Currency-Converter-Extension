470ffa3a9bbd0a4d95f9e1ead797887561a1670c
cche0258 <cche0258@student.monash.edu>
2025-01-23 11:08:29 +0800

    fetch api

---
+++ exchange.js
@@ -0,0 +1,33 @@
+const amount = document.getElementById('amount');
+const currency = document.getElementById('currency');
+const convert = document.getElementById('convert');
+const result = document.getElementById('result');
+
+const API_KEY="Yo3wpLsMrrNSAwwu9zI6lA==JLjD8cuKTqlPDhM9"
+
+const apiURL="https://api.api-ninjas.com/v1/exchangerate?pair=GBP_"
+
+convert.addEventListener('click', () => {
+    const amountTotal = amount.value;
+    const currencyTotal = currency.value;
+    const url = apiURL + currencyTotal;
+
+    fetch(url, {
+        headers: {
+            "X-Api-Key": API_KEY
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const rate = data.rate;
+        const result = amountTotal * rate;
+        result.innerHTML = `${amount} ${currency} = ${result.toFixed(2)} USD`;
+    })
+    .catch(error => {
+        console.error("Failed", error);
+        result.innerHTML = "An error occurred";
+    })
+})
+
+
+
